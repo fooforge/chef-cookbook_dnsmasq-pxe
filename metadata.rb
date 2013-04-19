1a3bc2d175af4868e51ac0b45ce89f0846018169
Mike Adolphs <mike@github.com>
2013-04-20 01:54:35 +0200

    Add metadata

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+ame              "dnsmasq-pxe"
+maintainer       "Mike Adolphs"
+maintainer_email "mike@fooforge.com"
+license          "Apache 2.0"
+description      "Chef cookbook for configuring dnsmask to only answer PXE requests"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+recommends       "pxe_dust", ">= 1.4.1"
+
+%w{ ubuntu debian }.each do |os|
+  supports os
+end
