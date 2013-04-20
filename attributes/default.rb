3ef0306135ce600bfa52516c5362804b868b70f3
Mike Adolphs <mike@github.com>
2013-04-20 02:01:00 +0200

    Stringify IP addresses

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default[:dnsmasq_pxe][:network_range]        = 192.168.1.0
-default[:dnsmasq_pxe][:tftp_server_ip]       = 192.168.1.1
+default[:dnsmasq_pxe][:network_range]        = "192.168.1.0"
+default[:dnsmasq_pxe][:tftp_server_ip]       = "192.168.1.1"
