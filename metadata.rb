a2d1b6c265057f8fa10eb4b713d067a5127485ab
Mike Adolphs <mike@github.com>
2013-04-20 02:03:30 +0200

    Raise version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "mike@fooforge.com"
 license          "Apache 2.0"
 description      "Chef cookbook for configuring dnsmask to only answer PXE requests"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 recommends       "pxe_dust", ">= 1.4.1"
 
 %w{ ubuntu debian }.each do |os|
